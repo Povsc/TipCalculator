bc786dfd70359021ed719d5138229050d8308cfd
Felipe Conte Cortez Martins <felipeccm@fb.com>
2021-06-22 15:15:37 -0400

    Initial Commit

---
+++ TipCalculator/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  TipCalculator
+//
+//  Created by felipeccm on 6/22/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by felipeccm on 6/22/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
