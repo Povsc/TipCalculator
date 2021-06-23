81dd684e5c8877e1cb9e6229233b12db2136a6bd
Felipe Conte Cortez Martins <felipeccm@fb.com>
2021-06-22 21:06:15 -0400

    Changed UI

--- TipCalculator/TipViewController.m
+++ TipCalculator/TipViewController.m
@@ -12,19 +12,30 @@
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *tipPercentageControl;
+@property (weak, nonatomic) IBOutlet UITextField *tipTextLabel;
+@property (weak, nonatomic) IBOutlet UITextField *totalTextLabel;
+
 
 @end
 
 @implementation TipViewController
 
+BOOL lowered = true;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
-- (IBAction)onTap:(id)sender {
-    [self.view endEditing:true];
+    [self.billField becomeFirstResponder];
 }
 - (IBAction)updateLabel:(id)sender {
+    if (self.billField.text.length == 0 && lowered == false){
+        [self hideLabels];
+        lowered = true;
+    }
+    
+    if (self.billField.text.length == 1 && lowered == true){
+        [self showLabels];
+        lowered = false;
+    }
     double tipPercentages[] = {0.15, 0.2, 0.25};
     double tipPercentage = tipPercentages[self.tipPercentageControl.selectedSegmentIndex];
     
@@ -34,6 +45,73 @@
     
     self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
     self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+}
+
+- (void)hideLabels {
+    [UIView animateWithDuration:0.5 animations:^{
+        CGRect billFrame = self.billField.frame;
+        billFrame.origin.y += 200;
+        self.billField.frame = billFrame;
+        
+        CGRect tipFrame = self.tipLabel.frame;
+        tipFrame.origin.y += 200;
+        self.tipLabel.frame = tipFrame;
+        self.tipLabel.hidden = true;
+        
+        CGRect tipTextFrame = self.tipTextLabel.frame;
+        tipTextFrame.origin.y += 200;
+        self.tipTextLabel.frame = tipTextFrame;
+        self.tipTextLabel.hidden = true;
+        
+        CGRect totalFrame = self.totalLabel.frame;
+        totalFrame.origin.y += 200;
+        self.totalLabel.frame = totalFrame;
+        self.totalLabel.hidden = true;
+        
+        CGRect totalTextFrame = self.totalTextLabel.frame;
+        totalTextFrame.origin.y += 200;
+        self.totalTextLabel.frame = totalTextFrame;
+        self.totalTextLabel.hidden = true;
+        
+        CGRect percentageFrame = self.tipPercentageControl.frame;
+        percentageFrame.origin.y += 200;
+        self.tipPercentageControl.frame = percentageFrame;
+        self.tipPercentageControl.hidden = true;
+    }];
+    
+}
+
+- (void)showLabels {
+    [UIView animateWithDuration:0.5 animations:^{
+        CGRect billFrame = self.billField.frame;
+        billFrame.origin.y -= 200;
+        self.billField.frame = billFrame;
+        
+        CGRect tipFrame = self.tipLabel.frame;
+        tipFrame.origin.y -= 200;
+        self.tipLabel.frame = tipFrame;
+        self.tipLabel.hidden = false;
+        
+        CGRect tipTextFrame = self.tipTextLabel.frame;
+        tipTextFrame.origin.y += 200;
+        self.tipTextLabel.frame = tipTextFrame;
+        self.tipTextLabel.hidden = true;
+        
+        CGRect totalFrame = self.totalLabel.frame;
+        totalFrame.origin.y -= 200;
+        self.totalLabel.frame = totalFrame;
+        self.totalLabel.hidden = false;
+        
+        CGRect totalTextFrame = self.totalTextLabel.frame;
+        totalTextFrame.origin.y -= 200;
+        self.totalTextLabel.frame = totalTextFrame;
+        self.totalTextLabel.hidden = false;
+        
+        CGRect percentageFrame = self.tipPercentageControl.frame;
+        percentageFrame.origin.y -= 200;
+        self.tipPercentageControl.frame = percentageFrame;
+        self.tipPercentageControl.hidden = false;
+    }];
 }
 
 /*
